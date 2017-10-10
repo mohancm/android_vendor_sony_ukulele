4c2a3f93067e1f596ccc207985195bf9df46a209
Mohan CM <mohanmanjappa@gmail.com>
2017-10-10 12:51:18 +0530

    Android 7.0 Stock
    Signed-off-by: Mohan CM <mohanmanjappa@gmail.com>

---
+++ ukulele-vendor-blobs.mk
@@ -0,0 +1,22 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/ukulele/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/ukulele/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/ukulele/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/ukulele/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/ukulele/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/ukulele/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/ukulele/vendor/bin,system/vendor/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/ukulele/vendor/lib,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/ukulele/vendor/lib64,system/vendor/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/ukulele/vendor/etc,system/vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/ukulele/vendor/firmware,system/vendor/firmware)
+
